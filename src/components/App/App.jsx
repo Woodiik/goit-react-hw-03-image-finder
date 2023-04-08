21324819eb64918ce33ae522bcda4927a14d35d0
Woodiik <vovikvova04@gmail.com>
2023-04-08 22:48:39 +0300

    Summary without state machine

---
+++ src/components/App/App.jsx
@@ -0,0 +1,87 @@
+import { Button } from 'components/Button/Button';
+import { Gallery } from 'components/ImageGallery/ImageGallery';
+import { Item } from 'components/ImageGalleryItem/ImageGalleryItem';
+import { Loader } from 'components/Loader/Loader';
+import { Modal } from 'components/Modal/Modal';
+import { SearchBar } from 'components/Searchbar/SearchBar';
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    name: '',
+    items: null,
+    page: 1,
+    loader: false,
+    showModal: false,
+    currentImage: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.name !== this.state.name) {
+      const { name, page } = this.state;
+      this.setState({ loader: true });
+      fetch(
+        `https://pixabay.com/api/?q=${name}&page=${page}&key=33842320-ed19ffa83cc28946150fb442a&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(res => res.json())
+        .then(({ hits }) => {
+          this.setState(prevState => {
+            return { items: hits, loader: false, page: prevState.page + 1 };
+          });
+        });
+    }
+  }
+  onSubmit = name => {
+    this.setState({ name });
+  };
+  onButtonClick = () => {
+    const { name, page } = this.state;
+    this.setState({ loader: true });
+    fetch(
+      `https://pixabay.com/api/?q=${name}&page=${page}&key=33842320-ed19ffa83cc28946150fb442a&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(res => res.json())
+      .then(({ hits }) => {
+        this.setState(prevState => {
+          return {
+            items: [...prevState.items, ...hits],
+            page: prevState.page + 1,
+            loader: false,
+          };
+        });
+      });
+  };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+  onItemClick = url => {
+    this.setState({ currentImage: url });
+  };
+  render() {
+    const { items, loader, showModal, currentImage } = this.state;
+
+    return (
+      <div>
+        <SearchBar onSubmit={this.onSubmit} />
+        {showModal && <Modal onClose={this.toggleModal} url={currentImage} />}
+        <Gallery>
+          {items &&
+            items.map(({ id, largeImageURL, webformatURL }) => {
+              return (
+                <Item
+                  key={id}
+                  url={webformatURL}
+                  largeImg={largeImageURL}
+                  openModal={this.toggleModal}
+                  onClick={this.onItemClick}
+                />
+              );
+            })}
+        </Gallery>
+        {loader && <Loader />}
+        {items?.length > 0 && <Button onClick={this.onButtonClick} />}
+      </div>
+    );
+  }
+}
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+  handleKeydown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div className="overlay">
+        <div className="modal">
+          <img src={this.props.url} alt="" />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  url: PropTypes.string.isRequired,
+};
