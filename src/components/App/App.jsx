1742bcfc1537e3e33489496188c6ce5faa57bb14
Woodiik <vovikvova04@gmail.com>
2023-04-09 12:16:30 +0300

    Remove state machine, add servises.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import { Item } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Loader } from 'components/Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
 import { SearchBar } from 'components/Searchbar/SearchBar';
+import { fetchPhotos } from 'components/servises/fetch';
 import { Component } from 'react';
 export class App extends Component {
   state = {
@@ -12,22 +13,19 @@ export class App extends Component {
     page: 1,
     showModal: false,
     currentImage: '',
-    status: 'idle',
+    loader: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.name !== this.state.name) {
       const { name, page } = this.state;
-      this.setState({ status: 'pending' });
-      fetch(
-        `https://pixabay.com/api/?q=${name}&page=${page}&key=33842320-ed19ffa83cc28946150fb442a&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(res => res.json())
+      this.setState({ loader: true });
+      fetchPhotos(name, page)
         .then(({ hits }) => {
           this.setState(prevState => {
             return {
               items: hits,
-              status: 'resolved',
+              loader: false,
               page: prevState.page + 1,
             };
           });
@@ -40,20 +38,16 @@ export class App extends Component {
   };
   onButtonClick = () => {
     const { name, page } = this.state;
-    this.setState({ status: 'pending' });
-    fetch(
-      `https://pixabay.com/api/?q=${name}&page=${page}&key=33842320-ed19ffa83cc28946150fb442a&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(res => res.json())
-      .then(({ hits }) => {
-        this.setState(prevState => {
-          return {
-            items: [...prevState.items, ...hits],
-            page: prevState.page + 1,
-            status: 'resolved',
-          };
-        });
+    this.setState({ loader: true });
+    fetchPhotos(name, page).then(({ hits }) => {
+      this.setState(prevState => {
+        return {
+          items: [...prevState.items, ...hits],
+          page: prevState.page + 1,
+          loader: false,
+        };
       });
+    });
   };
   toggleModal = () => {
     this.setState(({ showModal }) => ({
@@ -64,53 +58,20 @@ export class App extends Component {
     this.setState({ currentImage: url });
   };
   render() {
-    const { items, showModal, currentImage, name, status } = this.state;
+    const { items, showModal, currentImage, name, loader } = this.state;
 
-    if (status === 'idle') {
-    }
-    if (status === 'pending') {
-      return <Loader />;
-    }
-    if (status === 'rejected') {
-      return (
-        <h1 className="error-text">
-          We could not find the photos for the request '{name}'
-        </h1>
-      );
-    }
-    if (status === 'resolved') {
-      return (
-        <div>
-          <Gallery>
-            {items.map(({ id, largeImageURL, webformatURL }) => {
-              return (
-                <Item
-                  key={id}
-                  url={webformatURL}
-                  largeImg={largeImageURL}
-                  openModal={this.toggleModal}
-                  onClick={this.onItemClick}
-                />
-              );
-            })}
-          </Gallery>
-          <Button onClick={this.onButtonClick} />;
-          {showModal && <Modal onClose={this.toggleModal} url={currentImage} />}
-        </div>
-      );
-    }
     return (
       <div>
         <SearchBar onSubmit={this.onSubmit} />
-        {/*{showModal && <Modal onClose={this.toggleModal} url={currentImage} />}*/}
+        {showModal && <Modal onClose={this.toggleModal} url={currentImage} />}
 
-        {/*{items?.length === 0 && (
+        {items?.length === 0 && (
           <h1 className="error-text">
             We could not find the photos for the request '{name}'
           </h1>
-        )}*/}
-        {/*<Gallery>*/}
-        {/*{items &&
+        )}
+        <Gallery>
+          {items &&
             items.map(({ id, largeImageURL, webformatURL }) => {
               return (
                 <Item
@@ -121,10 +82,10 @@ export class App extends Component {
                   onClick={this.onItemClick}
                 />
               );
-            })}*/}
-        {/*</Gallery>*/}
-        {/*{loader && <Loader />}*/}
-        {/*{items?.length > 0 && <Button onClick={this.onButtonClick} />}*/}
+            })}
+        </Gallery>
+        {loader && <Loader />}
+        {items?.length > 0 && <Button onClick={this.onButtonClick} />}
       </div>
     );
   }
